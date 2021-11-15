72e4019e3ab305747f41b46f0531b43a979413b1
Julien <julien.fieschi@afsr.fr>
2021-11-15 21:31:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,10 @@ function requireHTTPS(req, res, next) {
 }
 const express = require('express');
 const app = express();
-
 app.use(requireHTTPS);
-
 app.use(express.static('./dist/InterAACtionScene'));
-
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/InterAACtionScene/'}
 );
 });
-
 app.listen(process.env.PORT || 8080);
